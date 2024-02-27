bfdd33baa5964b8f041d576c61f8c11e9252e1da
zavch <2294883873@qq.com>
2024-02-27 17:33:37 +0800

    feat(批量访问地址): 增加package-lock.json内地址访问

--- src/main/java/com/github/zavch/gear/ToolViewController.java
+++ src/main/java/com/github/zavch/gear/ToolViewController.java
@@ -1,13 +1,32 @@
 package com.github.zavch.gear;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.*;
 import java.util.Base64;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ToolViewController {
+    private static final String PACKAGE_LOCK_JSON = "package-lock.json";
+    private static final String REGEX = ".*\"resolved\": \"(.*)\",.*";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
     @FXML
     private TextArea jwt;
     @FXML
@@ -24,6 +43,14 @@ public class ToolViewController {
     private Spinner<Integer> second;
     @FXML
     private Spinner<Integer> millisecond;
+    @FXML
+    private ComboBox<String> fileType;
+    @FXML
+    private Button fileChooseButton;
+    @FXML
+    private ProgressBar progressBar;
+    private File file;
+    private ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
     @FXML
     public void parseJwt(ActionEvent actionEvent) {
@@ -43,6 +70,13 @@ public class ToolViewController {
 
     private static void showErrorAlert(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    private static void showInfoAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.show();
@@ -85,5 +119,71 @@ public class ToolViewController {
         } catch (Exception e) {
             showErrorAlert("时间戳格式错误");
         }
+    }
+
+    @FXML
+    public void access(ActionEvent actionEvent) throws IOException {
+        if (fileType.getValue() != null && file != null) {
+            switch (fileType.getValue()) {
+                case PACKAGE_LOCK_JSON:
+                    Task<Void> task = new Task<>() {
+                        @Override
+                        protected Void call() throws Exception {
+                            try {
+                                List<String> urls = Files.readAllLines(Paths.get(file.getAbsolutePath()))
+                                        .stream()
+                                        .map(PATTERN::matcher)
+                                        .filter(Matcher::matches)
+                                        .map(m -> m.group(1))
+                                        .collect(Collectors.toList());
+                                int total = urls.size();
+                                AtomicInteger atomicInteger = new AtomicInteger();
+                                urls.parallelStream().forEach(u -> {
+                                    try {
+                                        URL url = new URL(u);
+                                        URLConnection urlConnection = url.openConnection();
+                                        urlConnection.connect();
+                                    } catch (Exception e) {
+                                        System.err.println(e.getMessage());
+                                    } finally {
+                                        int current = atomicInteger.incrementAndGet();
+                                        updateProgress(current, total);
+                                        if (current == total) {
+                                            progressBar.setVisible(false);
+                                            Platform.runLater(() -> {
+                                                showInfoAlert("批量访问结束");
+                                            });
+                                        }
+                                    }
+                                });
+                            } catch (Exception e) {
+                                System.err.println(e.getMessage());
+                            }
+                            return null;
+                        }
+                    };
+                    progressBar.progressProperty().bind(task.progressProperty());
+                    progressBar.setVisible(true);
+                    threadPool.submit(task);
+                    break;
+            }
+        }
+    }
+
+    @FXML
+    public void chooseFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        if (file != null) {
+            fileChooser.setInitialDirectory(file.getParentFile());
+        }
+        file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            fileChooseButton.setText(file.getName());
+        }
+    }
+
+    public void initialize() {
+        fileType.getItems().addAll(PACKAGE_LOCK_JSON);
+        fileType.setValue(PACKAGE_LOCK_JSON);
     }
 }
